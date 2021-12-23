fbd6a598304296b865dd89b3363d1eafa811e40f
Ontorok <nasir.amath07@gmail.com>
2021-12-23 11:16:40 +0600

    movie item add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,20 @@
-const navLinks = document.querySelector('.nav-links')
+const arrows = document.querySelectorAll('.arrow');
+const movies = document.querySelectorAll('.movie-list')
 
-function showMenu() {
-    navLinks.style.right = '0px'
-}
+arrows.forEach((arrow, index) => {
+    const itemCount = movies[index].querySelectorAll('img').length;
+    let clickCounter = 0;
 
-function hideMenu() {
-    navLinks.style.right = '-200px'
-}
+    arrow.addEventListener('click', () => {
+        clickCounter++;
+        if (itemCount - (4 + clickCounter) >= 0) {
+            movies[index].style.transform = `translateX(${movies[index].computedStyleMap().get('transform')[0].x.value - 300}px)`;
+        } else {
+            movies[index].style.transform = `translateX(0)`;
+            clickCounter = 0;
+        }
+
+    });
+
+    // console.log()
+})
