38b1802358e221776007a41b9e1f626b0adbbb2a
Ontorok <nasir.amath07@gmail.com>
2021-12-23 11:57:24 +0600

    item with window size add

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,37 @@
 const arrows = document.querySelectorAll('.arrow');
 const movies = document.querySelectorAll('.movie-list')
+let clickCounter = 0;
+let itemInWindow = 0;
+const screenWidth = window.screen.width
+
+switch (screenWidth) {
+    case 1920:
+        itemInWindow = 6;
+        break;
+    case 1280:
+        itemInWindow = 4;
+        break;
+    case 1024:
+        itemInWindow = 3;
+        break;
+    case 768:
+        itemInWindow = 2;
+        break;
+    default:
+        itemInWindow = 1;
+        break;
+}
 
 arrows.forEach((arrow, index) => {
     const itemCount = movies[index].querySelectorAll('img').length;
-    let clickCounter = 0;
-
     arrow.addEventListener('click', () => {
         clickCounter++;
-        if (itemCount - (4 + clickCounter) >= 0) {
+        if (itemCount - (itemInWindow + clickCounter) >= 0) {
             movies[index].style.transform = `translateX(${movies[index].computedStyleMap().get('transform')[0].x.value - 300}px)`;
         } else {
             movies[index].style.transform = `translateX(0)`;
             clickCounter = 0;
         }
-
     });
+});
 
-    // console.log()
-})
