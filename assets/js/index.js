8776cea81e3584536dc2a672f940e7cc1262a384
Ontorok <nasir.amath07@gmail.com>
2021-12-29 17:15:01 +0600

    add responsiveness for item

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,15 @@
 const arrows = document.querySelectorAll('.arrow');
 const movies = document.querySelectorAll('.movie-list')
 let clickCounter = 0;
-let itemInWindow = 0;
+
 const screenWidth = window.screen.width
 
-switch (screenWidth) {
-    case 1920:
-        itemInWindow = 6;
-        break;
-    case 1280:
-        itemInWindow = 4;
-        break;
-    case 1024:
-        itemInWindow = 3;
-        break;
-    case 768:
-        itemInWindow = 2;
-        break;
-    default:
-        itemInWindow = 1;
-        break;
-}
+
 
 arrows.forEach((arrow, index) => {
     const itemCount = movies[index].querySelectorAll('img').length;
     arrow.addEventListener('click', () => {
+        let itemInWindow = Math.floor(window.innerWidth / 270)
         clickCounter++;
         if (itemCount - (itemInWindow + clickCounter) >= 0) {
             movies[index].style.transform = `translateX(${movies[index].computedStyleMap().get('transform')[0].x.value - 300}px)`;
